9bedbefd5871b2fdc06e4a76a4a6f05339567a9e
Md.Antonin Islam <md.antonin686.pro@gmail.com>
2022-08-11 21:02:42 +0600

    Updates student service
    changed service's return type to match API documentation.

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -4,14 +4,40 @@ import { DBService } from 'src/database/db.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 
+interface Iresponse {
+  status: string;
+  messages: string[];
+  data?: Student;
+}
+
 @Injectable()
 export class StudentsService {
   constructor(private dbService: DBService) {}
 
-  create(createStudentDto: CreateStudentDto) {
-    return this.dbService.student.create({
-      data: createStudentDto,
-    });
+  async create(createStudentDto: CreateStudentDto) {
+    let data: Student = null;
+    let messages: string[] = [];
+    let status: string = "";
+
+    try {
+      data = await this.dbService.student.create({
+        data: createStudentDto,
+      });
+      messages.push('Student record successfully created');
+      status = 'success';
+    } catch (error) {
+      messages.push('Student record cannot be created');
+      status = 'failed';
+    } finally {
+      let response: Iresponse = {
+        status: status,
+        messages: messages,
+      };
+
+      response = data ? { ...response, data: data } : response;
+
+      return response;
+    }
   }
 
   findAll() {
@@ -26,20 +52,58 @@ export class StudentsService {
     });
   }
 
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return this.dbService.student.update({
-      where: {
-        id: id,
-      },
-      data: updateStudentDto,
-    });
+  async update(id: number, updateStudentDto: UpdateStudentDto) {
+    let data: Student = null;
+    let messages: string[] = [];
+    let status: string = "";
+
+    try {
+      data = await this.dbService.student.update({
+        where: {
+          id: id,
+        },
+        data: updateStudentDto,
+      });
+      messages.push('Student record successfully updated');
+      status = 'success';
+    } catch (error) {
+      messages.push('Student record cannot be updated or found');
+      status = 'failed';
+    } finally {
+      let response: Iresponse = {
+        status: status,
+        messages: messages,
+      };
+
+      response = data ? { ...response, data: data } : response;
+
+      return response;
+    }
   }
 
-  remove(id: number) {
-    return this.dbService.student.delete({
-      where: {
-        id: id,
-      },
-    });
+  async remove(id: number) {
+    let data: Student = null;
+    let messages: string[] = [];
+    let status: string = "";
+
+    try {
+      data = await this.dbService.student.delete({
+        where: {
+          id: id,
+        },
+      });
+      messages.push('Student record successfully deleted');
+      status = 'success';
+    } catch (error) {
+      messages.push('Student record cannot be deleted or found');
+      status = 'failed';
+    } finally {
+      let response: Iresponse = {
+        status: status,
+        messages: messages,
+      };
+
+      return response;
+    }
   }
 }

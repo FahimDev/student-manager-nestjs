585bbca4431cdd2b5e18a75b37e66aed8cf6b4be
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-08-12 20:06:18 +0600

    User password update feature added.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -25,6 +27,12 @@ export class UsersController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Patch('update-password/:username')
+  updatePassword(@Param('username') username: string, @Body() updateUserDto: UpdateUserDto) {
+    console.log("updateUserDto")
+    return this.usersService.updatePassword(username, updateUserDto);
   }
 
   @Delete(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,20 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { DBService } from 'src/database/db.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private dbService: DBService) {}
-  
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(username: string) {
+  async findOne(username: string) {
     return this.dbService.user.findFirst({
       where: {
         username: username,
@@ -25,6 +19,54 @@ export class UsersService {
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
+  }
+
+  async updatePassword(username: string, updateUserDto: UpdateUserDto) {
+    let data: User = null;
+    let messages: string[] = [];
+    let status: string = '';
+      try {
+        const user = await this.dbService.user.findFirst({
+          where: {
+            username: username,
+          },
+        });
+        console.log(user);
+        const isMatch = await bcrypt.compare(updateUserDto.oldPassword, user.password);    // Authenticating Password.
+        console.log(isMatch);
+        if (isMatch) {
+          // Password encryption Ref: https://docs.nestjs.com/security/encryption-and-hashing
+          const salt = await bcrypt.genSalt();    // Generating Salt for encrypting Password.
+          const newHashedPassword = await bcrypt.hash(updateUserDto.newPassword, salt);    // Plain text password hashed.
+          data = await this.dbService.user.update({
+            where: {
+              id: user.id,
+            },
+            data: {
+              username: user.username,
+              password: newHashedPassword,
+              role: user.role,
+              email: user.email,
+            }, 
+          });
+          messages.push('User password updated successfully');
+          status = 'success';
+        } else {
+          messages.push('Sorry, curent password is wrong!');
+          status = 'failed';
+        }
+      } catch (error) {
+        messages.push('Password was not updated!');
+        status = 'failed';
+      } finally {
+        // Following common standard for API response.
+        let response: Iresponse = {
+          status: status,
+          messages: messages,
+        };
+        response = data ? { ...response, data: data } : response;
+        return response;
+      } 
   }
 
   remove(id: number) {

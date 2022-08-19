b89e787a3c5ae1940fcd92889ff38c69e655463a
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2022-08-19 16:48:56 +0600

    Admin CRUD unit test successful.

--- src/admins/admins.controller.spec.ts
+++ src/admins/admins.controller.spec.ts
@@ -4,9 +4,12 @@ import { AdminsService } from './admins.service';
 import { DBService } from '../database/db.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { faker } from '@faker-js/faker';
+import { UpdateAdminDto } from './dto/update-admin.dto';
 
 describe('AdminsController', () => {
   let controller: AdminsController;
+  let adminId:number;
+  let adminInfo: CreateAdminDto;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,7 +25,6 @@ describe('AdminsController', () => {
   });
 
   //https://fakerjs.dev/guide/
-  describe('create', () => {
     it('should create new admin', async () => {
       const newAdmin = {
         name: faker.name.fullName(),
@@ -33,19 +35,54 @@ describe('AdminsController', () => {
         role: 'ADMIN',
         designation: faker.name.jobArea(),
       } as CreateAdminDto;
-      console.log(newAdmin)
       const response = await controller.create(newAdmin);
       expect(response.status).toEqual('success');
       expect(response.data).toBeDefined();
+      adminId = response.data['id'];
+      adminInfo = newAdmin;
     });
-  });
 
-  describe('findAll', () => {
     it('should find all admins', async () => {
       const response = await controller.findAll();
       expect(response).toBeDefined();
       expect(response.length).toBeGreaterThan(0);
     });
+
+    it('should find the recently created admin', async () => {
+      const response = await controller.findOne(String(adminId));
+      expect(response).toBeDefined();
+    });
+
+    
+  it('should update the recently created admin info', async () => {
+    const updateStudent = {
+      name: faker.name.fullName(),
+      phone: faker.random.numeric(11),
+      designation: faker.name.jobArea(),
+    } as UpdateAdminDto;
+    const response = await controller.update(String(adminId), updateStudent);
+    expect(response.status).toEqual('success');
+    console.log(`
+    ---------Current Admin information---------
+    ${adminInfo.name}
+    ---------Updated Admin information---------
+    ${updateStudent.name}
+    --------------------END--------------------
+    `);
+    adminInfo.name = updateStudent.name;
+    adminInfo.phone = updateStudent.phone;
+    adminInfo.designation = updateStudent.designation;
+
+  });
+
+  it('should delete recently updated admin', async () => {
+    const response = await controller.remove(String(adminId));
+    expect(response.status).toEqual('success');
+    console.log(`
+    ---------Admin removed successfully---------
+    ${adminInfo.name}
+    --------------------END---------------------
+    `);
   });
 
 });

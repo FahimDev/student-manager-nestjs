2dc0b0485becc1a0fe668d7c311cd38479ebd220
Md.Antonin Islam <md.antonin686.pro@gmail.com>
2022-08-23 19:16:33 +0600

    Adds service mocking to controller Unit Tests
    Instead of using Database and ProgramService we will now mocked service.

--- src/programs/programs.controller.spec.ts
+++ src/programs/programs.controller.spec.ts
@@ -1,10 +1,78 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { DBService } from '../database/db.service';
+import { Test } from '@nestjs/testing';
 import { CreateProgramDto } from './dto/create-program.dto';
 import { UpdateProgramDto } from './dto/update-program.dto';
 import { ProgramsController } from './programs.controller';
 import { ProgramsService } from './programs.service';
-import { faker } from '@faker-js/faker';
+
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
+
+const createResult = {
+  status: 'success',
+  messages: ['Program record successfully created'],
+  data: {
+    id: 2,
+    title: 'Commerce',
+    description: 'text',
+    type: 'COMMERCE',
+  },
+};
+
+const findAllResult = {
+  status: 'success',
+  messages: ['Program record(s) successfully found'],
+  data: [
+    {
+      id: 1,
+      title: 'Science',
+      description: 'text',
+      type: 'SCIENCE',
+    },
+  ],
+};
+
+const findOneResult = {
+  status: 'success',
+  messages: ['Requested student\'s record successfully found'],
+  data: {
+    id: 2,
+    title: 'Commerce',
+    description: 'text',
+    type: 'COMMERCE',
+  },
+};
+
+const findStudentsResults = {
+  status: 'success',
+  messages: ['Program\'s student record(s) successfully found'],
+  data: [
+    {
+      id: 1,
+      name: 'Antonin',
+      email: 'antonin@gmail.com',
+      phone: '016',
+      address: 'Dhaka',
+      programId: 1
+    },
+  ],
+}
+
+const updateResult = {
+  status: 'success',
+  messages: ['Program record successfully updated'],
+  data: {
+    id: 2,
+    title: 'Commerce',
+    description: 'Modified description',
+    type: 'COMMERCE',
+  },
+}
+
+const deleteResult = {
+  status: 'success',
+  messages: ['Program record successfully deleted'],
+}
 
 describe('ProgramsController', () => {
   let controller: ProgramsController;
@@ -12,12 +80,33 @@ describe('ProgramsController', () => {
   let programTitle: string;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    
+    const moduleRef = await Test.createTestingModule({
       controllers: [ProgramsController],
-      providers: [ProgramsService, { provide: DBService, useClass: DBService }],
-    }).compile();
+    })
+      .useMocker((token) => { // token are the classes called in controller
+        if (token === ProgramsService) {
+          
+          return {
+            create: jest.fn().mockResolvedValue(createResult),
+            findAll: jest.fn().mockResolvedValue(findAllResult),
+            findOne: jest.fn().mockResolvedValue(findOneResult),
+            findStudents: jest.fn().mockResolvedValue(findStudentsResults),
+            update: jest.fn().mockResolvedValue(updateResult),
+            remove: jest.fn().mockResolvedValue(deleteResult),
+          };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
-    controller = module.get<ProgramsController>(ProgramsController);
+    controller = moduleRef.get(ProgramsController);
   });
 
   it('should be defined', () => {
@@ -25,15 +114,10 @@ describe('ProgramsController', () => {
   });
 
   it('should create new program', async () => {
-    const programInfo = [
-      { title: 'Arts', type: 'ARTS' },
-      { title: 'Commerce', type: 'COMMERCE' },
-    ];
-    const randomNum = Math.floor(Math.random() * 2);
     const newProgram = {
-      title: programInfo[randomNum].title,
-      description: faker.random.words(5),
-      type: programInfo[randomNum].type,
+      title: 'Commerce',
+      description: 'text',
+      type: 'COMMERCE',
     } as CreateProgramDto;
 
     const response: any = await controller.create(newProgram);
@@ -47,12 +131,14 @@ describe('ProgramsController', () => {
 
   it('should find all programs', async () => {
     const response: any = await controller.findAll();
+
     expect(response.status).toEqual('success');
     expect(response.data.length).toBeGreaterThan(0);
   });
 
   it('should find one program', async () => {
     const response: any = await controller.findOne(programID);
+
     expect(response.status).toEqual('success');
     expect(response.data.title).toEqual(programTitle);
   });
@@ -64,7 +150,7 @@ describe('ProgramsController', () => {
 
   it('should update program', async () => {
     const updateProgram = {
-      description: faker.random.words(5),
+      description: 'Modified description',
     } as UpdateProgramDto;
     const response = await controller.update(programID, updateProgram);
     expect(response.status).toEqual('success');

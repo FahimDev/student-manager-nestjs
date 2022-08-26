b827b95787f669d4f030d7db977309bbeb99594c
Noman5237 <anonyman637@gmail.com>
2022-08-26 16:52:43 +0600

    seeding tasks refactored with function

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,14 @@
 import { faker } from '@faker-js/faker';
-import { PrismaClient, ProgramType, Role } from '@prisma/client';
+import { PrismaClient, ProgramType } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function main() {
+async function resetDatabase() {
   await prisma.student.deleteMany();
   await prisma.program.deleteMany();
+}
 
-  // Create a new program
+async function createMockPrograms() {
   await prisma.program.create({
     data: {
       id: 1,
@@ -16,21 +17,30 @@ async function main() {
       type: ProgramType.SCIENCE,
     },
   });
+}
 
-  // Create some new students
+async function createStudents() {
+  const students = [];
+
   for (let i = 0; i < 10; i++) {
     const firstName = faker.name.firstName();
     const lastName = faker.name.lastName();
-    await prisma.student.create({
-      data: {
-        name: faker.name.fullName({ firstName, lastName }),
-        email: faker.internet.email(firstName, lastName),
-        program_id: 1,
-        phone: faker.phone.number(),
-        address: faker.address.streetAddress(),
-      },
+    students.push({
+      name: faker.name.fullName({ firstName, lastName }),
+      email: faker.internet.email(firstName, lastName),
+      program_id: 1,
+      phone: faker.phone.number(),
+      address: faker.address.streetAddress(),
     });
   }
+
+  await prisma.student.createMany({ data: students });
+}
+
+async function main() {
+  await resetDatabase();
+  await createMockPrograms();
+  await createStudents();
 }
 
 main()
